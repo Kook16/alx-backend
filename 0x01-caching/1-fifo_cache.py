5ae487c59682acf56880c181a381ac2ca50cc368
Kook <123327622+Kook16@users.noreply.github.com>
2024-07-23 23:00:59 -0700

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,7 +6,7 @@ from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     """ FIFOCache defines a FIFO caching system """
-    
+
     def __init__(self):
         """ Initialize """
         super().__init__()
