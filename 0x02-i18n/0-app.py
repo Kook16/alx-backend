b7602b45c0fac517259d8e1654c0f2f5b409af31
kok <kok@ubuntu.ubuntu-domain>
2024-07-30 21:16:39 +0300

    updated task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''Basic Flask app'''
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,7 +10,3 @@ app = Flask(__name__)
 def home() -> str:
     '''A home route'''
     return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
